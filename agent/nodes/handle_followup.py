135b1e73fbcb9a1cc23a726cc8cbcc282b61ff19
Pruthviraj Rathod <125789327+pruthvi4344@users.noreply.github.com>
2026-04-08 22:59:38 -0400

    Implement error handling for MISTRAL_API_KEY
    Added error handling for missing MISTRAL_API_KEY.

--- agent/nodes/handle_followup.py
+++ agent/nodes/handle_followup.py
@@ -11,7 +11,7 @@ load_dotenv()
 api_key = os.getenv("MISTRAL_API_KEY")
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 
-
+# error handling for mistral api key
 if not api_key:
     raise ValueError(" MISTRAL_API_KEY is missing. Set it in your environment variables.")
 
